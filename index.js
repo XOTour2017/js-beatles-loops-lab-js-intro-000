3785b21e6befba5507ad81dbaf5b02889e4b97bf
Ali Haider <dutchland2013@hotmail.com>
2018-02-23 19:05:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 function theBeatlesPlay(musicians, instruments) {
-  var empty = [];
-  for (var i = 0; i < musicians.length; i++) {
-    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  var emptyArray = [];
+  for(var i=0; i < musicians.length; i++) {
+    emptyArray.push(`${(musicians[i])} plays ${(instruments[i])}`);
   }
-  return(empty);
+  return emptyArray;
 }
 
 function johnLennonFacts(facts) {
-  var strings = [];
-  for (var i = 0; i < facts.length; i++) {
-    strings.push(facts[i] + "!!!" )
-}
-  return(strings);
-}
-
-function iLoveTheBeatles(number) {
-  var empty_array = [];
-  do {
-    empty_array.push("I love the Beatles!")
-    number++;
-  } while (number < 15)
-  return empty_array;
-}
+  var i = 0;
+  while(i < facts.length) {
+    facts[i] + "!!!";
+  }
+  return facts;
+};
